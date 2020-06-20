24a78b9787247fbb7324685d8d65dbb9876258dc
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-20 15:09:51 -0400

    Inital commit

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/20/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
