c13e1b95d1b58fb6637c0c90a7096b17adae9c6d
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-20 15:43:54 -0400

    Clean Up Navigation Controller

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -26,12 +26,22 @@ class MainTabController: UITabBarController {
     /// Configures the view controllers
     func configureViewControllers() {
         
-        let feed = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: FeedController())
-        let explore = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: ExploreController())
-        let notifications = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: NotificationsController())
-        let conversations = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: ConversationsController())
+        let feed = FeedController()
+        let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"),
+                                                rootViewController: feed)
+        let explore = ExploreController()
+        let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"),
+                                                rootViewController: explore)
         
-        viewControllers = [feed, explore, notifications, conversations]
+        let notifications = NotificationsController()
+        let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"),
+                                                rootViewController: notifications)
+        
+        let conversations = ConversationsController()
+        let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"),
+                                                rootViewController: conversations)
+        
+        viewControllers = [nav1, nav2, nav3, nav4]
         
     }
     
