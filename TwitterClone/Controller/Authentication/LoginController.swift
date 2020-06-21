53dcfaed1217269e53b170b4638212d2f9080612
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-21 19:14:27 -0400

    Setting up login UI

---
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginController.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/21/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    // MARK: - Properties
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "TwitterLogo")
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+    }
+
+}
---
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/21/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+
+}
