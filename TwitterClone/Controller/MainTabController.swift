adab5817b449e00013cdc779c0134fa5b4e8934c
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-22 11:05:35 -0400

    User logged in persistence

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -7,30 +7,52 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
 
     // MARK: - Properties
     
     let actionButton: UIButton = {
-        
         let button = UIButton(type: .system)
         button.tintColor = .white
         button.backgroundColor = .twitterBlue
         button.setImage(UIImage(named: "new_tweet"), for: .normal)
         button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
         return button
-        
     }()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        configureUI()
-        configureViewControllers()
         
+        //logUserOut()
+        view.backgroundColor = .twitterBlue
+        authenticateUserAndConfigureUI()
+    }
+    
+    // MARK: - API
+    
+    func authenticateUserAndConfigureUI() {
+        if Auth.auth().currentUser == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true)
+            }
+        } else {
+            configureViewControllers()
+            configureUI()
+        }
+    }
+    
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("DEBUG: Failed to sign out with error: \(error.localizedDescription)")
+        }
     }
 
     // MARK: - Selectors
@@ -44,7 +66,6 @@ class MainTabController: UITabBarController {
     // MARK: - Helpers
 
     func configureUI() {
-        
         view.addSubview(actionButton)
         actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,
                             right: view.safeAreaLayoutGuide.rightAnchor,
@@ -53,7 +74,6 @@ class MainTabController: UITabBarController {
                             width: 56,
                             height: 56)
         actionButton.layer.cornerRadius = 56 / 2
-        
     }
 
     func configureViewControllers() {
