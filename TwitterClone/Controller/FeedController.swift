124111486e3e78d382b0128578a5df274ced8758
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-22 17:02:40 -0400

    Populate feed with tweets

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -21,6 +21,11 @@ class FeedController: UICollectionViewController {
         }
     }
     
+    private var tweets = [Tweet]() {
+        didSet { collectionView.reloadData() }
+    }
+    
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -34,7 +39,7 @@ class FeedController: UICollectionViewController {
     
     func fetchTweets() {
         TweetService.shared.fetchTweets(completion: { tweets in
-            print(tweets)
+            self.tweets = tweets
         })
     }
     
@@ -67,9 +72,11 @@ class FeedController: UICollectionViewController {
     
 }
 
+// MARK: - UICollectionViewDelegate/DataSource
+
 extension FeedController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return tweets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -77,6 +84,8 @@ extension FeedController {
         return cell
     }
 }
+
+// MARK: - UICollectionViewDelegateFlowLayout
 
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
