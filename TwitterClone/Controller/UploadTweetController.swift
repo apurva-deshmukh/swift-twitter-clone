16058b98f8706cddbd3adba8c9e6a631dc361b17
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-22 15:46:28 -0400

    Added caption TextView

--- TwitterClone/Controller/UploadTweetController.swift
+++ TwitterClone/Controller/UploadTweetController.swift
@@ -39,6 +39,8 @@ class UploadTweetController: UIViewController {
         return iv
     }()
     
+    private let captionTextView = CaptionTextView()
+    
     // MARK: - Lifecycle
     
     init(user: User) {
@@ -74,13 +76,22 @@ class UploadTweetController: UIViewController {
         view.backgroundColor = .white
         configureNavigationBar()
         
-        view.addSubview(profileImageView)
-        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                                left: view.leftAnchor,
-                                paddingTop: 16,
-                                paddingLeft: 16)
+        let stack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        
+        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                     left: view.leftAnchor,
+                     right: view.rightAnchor,
+                     paddingTop: 16,
+                     paddingLeft: 16,
+                     paddingRight: 16)
         
         profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+        
+        
     }
     
     func configureNavigationBar() {
