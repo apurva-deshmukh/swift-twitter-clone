9265a8f3f2054c48ac3ff97993c16db13c2a8a9a
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-22 16:25:50 -0400

    Setup CollectionView for feed

---
+++ TwitterClone/View/TweetCell.swift
@@ -0,0 +1,22 @@
+//
+//  TweetCell.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/22/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
