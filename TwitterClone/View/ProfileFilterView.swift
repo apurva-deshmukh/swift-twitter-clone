e09b5cf31953e94ac88a3e8043697dfe357d89b7
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-22 22:48:09 -0400

    Profile filters data model

--- TwitterClone/View/ProfileFilterView.swift
+++ TwitterClone/View/ProfileFilterView.swift
@@ -34,6 +34,8 @@ class ProfileFilterView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: reusableIdentifier)
+        let selectedIndexPath = IndexPath(row: 0, section: 0)
+        collectionView.selectItem(at: selectedIndexPath, animated: true, scrollPosition: .left)
         addSubview(collectionView)
         collectionView.addConstraintsToFillView(self)
     }
@@ -45,11 +47,13 @@ class ProfileFilterView: UIView {
 
 extension ProfileFilterView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return ProfileFilterOptions.allCases.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusableIdentifier, for: indexPath) as! ProfileFilterCell
+        let option = ProfileFilterOptions(rawValue: indexPath.row)
+        cell.option = option
         return cell
     }
 }
@@ -66,7 +70,8 @@ extension ProfileFilterView: UICollectionViewDelegate {
 
 extension ProfileFilterView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: frame.width / 3, height: frame.height)
+        let count = CGFloat(ProfileFilterOptions.allCases.count)
+        return CGSize(width: frame.width / count, height: frame.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
---
+++ TwitterClone/ViewModel/ProfileHeaderViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHeaderViewModel.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/22/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import Foundation
+
+enum ProfileFilterOptions: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+    
+    var description: String {
+        switch self {
+        case .tweets: return "Tweets"
+        case .replies: return "Tweets & Replies"
+        case .likes: return "Likes"
+        }
+    }
+}
