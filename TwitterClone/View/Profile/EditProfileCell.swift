c51563e11633fb46605cb5193c0af0f397b668e9
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-25 15:34:32 -0400

    Edit profile data model

---
+++ TwitterClone/View/Profile/EditProfileCell.swift
@@ -0,0 +1,27 @@
+//
+//  EditProfileCell.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/25/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .systemRed
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+}
---
+++ TwitterClone/ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  EditProfileViewModel.swift
+//  TwitterClone
+//
+//  Created by Apurva Deshmukh on 6/25/20.
+//  Copyright © 2020 Apurva Deshmukh. All rights reserved.
+//
+
+import Foundation
+
+enum EditProfileOptions: Int, CaseIterable {
+    case fullname
+    case username
+    case bio
+    
+    var description: String {
+        switch self {
+        case .fullname: return "Fullname"
+        case .username: return "Username"
+        case .bio: return "Bio"
+        }
+    }
+}
